0bb010c31480b15f17548861a2d9561cbb90c547
Akira Otaki <ohatakky@gmail.com>
2020-10-04 11:56:15 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-function blog() {
-  gcloud functions deploy BlogHTTP --runtime go113 --trigger-http --entry-point=BlogHandler --region=asia-northeast1
+function blog_function() {
+  gcloud functions deploy BlogHTTP --runtime go113 --trigger-http --entry-point=BlogHandler --region=asia-northeast1 --ingress-settings=internal-only
+}
+
+
+# https://cloud.google.com/scheduler/docs/http-target-auth?hl=ja#using-gcloud
+function blog_scheduler() {
+  gcloud scheduler jobs create http --schedule="every 15 mins" --uri=${BLOG_FUNCTION_URI} --oidc-service-account-email=${BLOG_FUNCTION_SERVICE_ACCOUNT}
 }
 
 $1
